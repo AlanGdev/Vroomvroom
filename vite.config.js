43c342fe79853459290864794f477f9e5c2c6f0f
Alan.Grolleau <alan.grolleau@gmail.com>
2025-06-24 14:08:20 +0200

    j'en sais rien!...

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/VroomTrack-Suivi-vehicule/', // ← ← ← ajoute cette ligne !
   plugins: [react()],
   server: {
     historyApiFallback: true,
